5319294769ad1378459a6293237a4f3aebda4013
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-21 17:45:31 +0100

    rendered image gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+    .map(galleryItem => 
+        `<div class="gallery_item">
+            <a class="gallery_link" href="${galleryItem.original}">
+                <img class="gallery_image" 
+                    src="${galleryItem.preview}"
+                    data-source="${galleryItem.original}"
+                    alt="${galleryItem.description}"/>
+            </a>
+        </div>`)
+    .join(" ");
+
+console.log(markup);
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
