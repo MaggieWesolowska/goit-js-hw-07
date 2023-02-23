635044c9a7134689f6d2e26ba784b1af5af72613
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-23 15:53:18 +0100

    added simpleLightbox to js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,7 +7,7 @@ const gallery = document.querySelector('.gallery');
 
 const markup = galleryItems
     .map(galleryItem => 
-        `<a class="gallery__item" href="javascript:void(0);" ${galleryItem.original}">
+        `<a class="gallery__item" href= ${galleryItem.original}">
              <img class="gallery__image" 
                 src="${galleryItem.preview}"
                 alt="${galleryItem.description}"/>
@@ -18,18 +18,48 @@ console.log(markup);
 
 gallery.insertAdjacentHTML("afterbegin", markup);
 
-gallery.addEventListener('click', (selectImage) => {
-    selectImage.preventDefault();
+gallery.addEventListener('click', (event) => {
+    event.target.preventDefault();
+    selectImage()
 });
 
+let lightbox = new SimpleLightbox('.gallery__item img', {
+    overlay: true,
+    overlayOpacity: 0.7,
+    spinner: true,
+    nav: true,
+    captions: true,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'original',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    captionClass: 'gallery__caption',
+    close: true,
+    closeText: 'x',
+    swipeClose: true,
+    showCounter: false,
+    animationSpeed: 250,
+    animationSlide: true,
+    preloading: true,
+    enableKeyboard: true,
+    loop: true,
+    rel: false,
+    docClose: true,
+    className: 'simple-lightbox',
+    widthRatio: 0.8,
+    heightRatio: 0.9,
+    throttleInterval: 1,
+    doubleTapZoom: 2,
+    fadeSpeed: 300,
+    focus: true,
+});
+
+
 function selectImage(event) {
-    if (event.target.tagName !== 'image') {
+    if (event.target.nodeName !== "IMG") {
         return;
     } else {
-        var lightbox = new SimpleLightbox('.gallery a', {
-            
-            
-            
-            /* options */ });
-    }
+        return lightbox;
+}
 }
