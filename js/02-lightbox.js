43191c36300165a7ed4c233ee7e77c55f725378b
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-23 14:49:17 +0100

    edited task2 js and markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+    .map(galleryItem => 
+        `<a class="gallery__item" href="javascript:void(0);" ${galleryItem.original}">
+             <img class="gallery__image" 
+                src="${galleryItem.preview}"
+                alt="${galleryItem.description}"/>
+        </a>`)
+    .join("");
+
+console.log(markup);
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+gallery.addEventListener('click', (selectImage) => {
+    selectImage.preventDefault();
+});
+
+function selectImage(event) {
+    if (event.target.tagName !== 'image') {
+        return;
+    } else {
+        var lightbox = new SimpleLightbox('.gallery a', {
+            
+            
+            
+            /* options */ });
+    }
+}
